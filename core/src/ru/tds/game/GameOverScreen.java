3065dc9c00373605815cc48c4244e89a582aa67e
Trushenkov Dmitry <diman28051999@gmail.com>
2018-10-21 21:42:32 +0300

    Added buttons "Try again" and "Menu". Added output of time at screen

--- core/src/ru/tds/game/GameOverScreen.java
+++ core/src/ru/tds/game/GameOverScreen.java
@@ -1,10 +1,21 @@
 package ru.tds.game;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 /**
  * Класс экрана "Игра закончена".
@@ -13,45 +24,95 @@ import com.badlogic.gdx.graphics.Texture;
  */
 public class GameOverScreen implements Screen {
 
-    private final SpaceGame spaceGame;
-    private OrthographicCamera camera;
-    private Texture gameOverImage;
+    private SpaceGame spaceGame;
+    private Stage stage;
+    private TextureAtlas atlas;
+    private Skin skin;
+    private Table table;
+    private TextButton buttonPlay, buttonExit;
+    private BitmapFont white, black;
+    private Label heading;
     private Texture background;
-    private Texture tryAgainImage;
+
+//    private Texture backgroundImage;
 
     GameOverScreen(SpaceGame spaceGame) {
         this.spaceGame = spaceGame;
-
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 480, 800);
-
-        background = new Texture("space.png");
-        gameOverImage = new Texture("GAMEOVER.png");
-        tryAgainImage = new Texture("TRYAGAIN.png");
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        camera.update();
-
-        spaceGame.spriteBatch.setProjectionMatrix(camera.combined);
-        spaceGame.spriteBatch.begin();
-        spaceGame.spriteBatch.draw(background, 0, 0);
-        spaceGame.spriteBatch.draw(gameOverImage, 480 / 2 - gameOverImage.getWidth() / 2, 800 / 2);
-        spaceGame.spriteBatch.draw(tryAgainImage, 480 / 2 - tryAgainImage.getWidth() / 2, 800 / 2 - gameOverImage.getHeight() / 2);
-        spaceGame.spriteBatch.end();
-
-        if (Gdx.input.justTouched()) {
-            spaceGame.setScreen(new GameScreen(spaceGame));
-        }
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
     public void show() {
+        background = new Texture("background2.jpg");
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
 
+        atlas = new TextureAtlas("ui/button.pack");
+        skin = new Skin(atlas);
+
+        table = new Table(skin);
+        table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        //creating fonts
+        white = new BitmapFont(Gdx.files.internal("font/white.fnt"), false);
+        black = new BitmapFont(Gdx.files.internal("font/black.fnt"), false);
+
+        //creating buttons
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.up = skin.getDrawable("button.up");
+        textButtonStyle.down = skin.getDrawable("button.down");
+        textButtonStyle.pressedOffsetX = 1;
+        textButtonStyle.pressedOffsetY = -1;
+        textButtonStyle.font = black;
+
+        //size of button
+        textButtonStyle.up.setMinWidth(200);
+        textButtonStyle.up.setMinHeight(40);
+        textButtonStyle.down.setMinWidth(200);
+        textButtonStyle.down.setMinHeight(40);
+
+        //button "Try Again"
+        buttonPlay = new TextButton("TRY AGAIN", textButtonStyle);
+        buttonPlay.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                ((Game) Gdx.app.getApplicationListener()).setScreen(new GameScreen(spaceGame));
+            }
+        });
+        buttonPlay.pad(20);
+
+        //button "Main Menu"
+        buttonExit = new TextButton("MENU", textButtonStyle);
+        buttonExit.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                ((Game) Gdx.app.getApplicationListener()).setScreen(new MainMenuScreen(spaceGame));
+            }
+        });
+        buttonExit.pad(20);
+
+        Label.LabelStyle headingStyle = new Label.LabelStyle(white, Color.WHITE);
+
+        heading = new Label("Your time: " + spaceGame.timeOfPlayerLive + " sec.", headingStyle);
+        heading.setFontScale((float) 1.5);
+
+        table.add(heading);
+        table.row();
+        table.getCell(heading).spaceBottom(50);
+        table.add(buttonPlay);
+        table.row();
+        table.getCell(buttonPlay).spaceBottom(15);
+        table.add(buttonExit);
+        table.setFillParent(true);
+        table.background(new TextureRegionDrawable(new TextureRegion(background)));
+        stage.addActor(table);
     }
+
 
     @Override
     public void resize(int width, int height) {
@@ -75,9 +136,10 @@ public class GameOverScreen implements Screen {
 
     @Override
     public void dispose() {
-        spaceGame.dispose();
-        gameOverImage.dispose();
-        background.dispose();
-        tryAgainImage.dispose();
+        stage.dispose();
+        atlas.dispose();
+        skin.dispose();
+        white.dispose();
+        black.dispose();
     }
 }
