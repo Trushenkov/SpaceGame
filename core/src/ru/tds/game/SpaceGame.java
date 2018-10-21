ee5e7c71776dc013bd7aa6eaf2c516d4d79916b0
Trushenkov Dmitry <diman28051999@gmail.com>
2018-10-21 21:44:33 +0300

    Added variable "timeOfPlayerLive" that save time of player. Created setter for this variable

--- core/src/ru/tds/game/SpaceGame.java
+++ core/src/ru/tds/game/SpaceGame.java
@@ -1,19 +1,26 @@
 package ru.tds.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class SpaceGame extends Game {
 
+    public static final String TITLE = "Space Game v1.0";
+
     SpriteBatch spriteBatch;
     BitmapFont font;
+    int timeOfPlayerLive = 0; //time from GameScreen
+
+    void setTimeOfPlayerLive(int timeOfPlayerLive) {
+        this.timeOfPlayerLive = timeOfPlayerLive;
+    }
 
     @Override
     public void create() {
         spriteBatch = new SpriteBatch();
-        font = new BitmapFont();
-        font.setColor(com.badlogic.gdx.graphics.Color.WHITE);
+        font = new BitmapFont(Gdx.files.internal("font/white.fnt"), false);
         this.setScreen(new MainMenuScreen(this));
     }
 
