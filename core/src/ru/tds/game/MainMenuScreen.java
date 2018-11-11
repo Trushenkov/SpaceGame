d2cb93be5b9d932be96019adeec0a18cc311eed4
Trushenkov Dmitry <diman28051999@gmail.com>
2018-11-11 19:36:15 +0300

    changed button size and added opportunity resize for different devices. refactoring done

--- core/src/ru/tds/game/MainMenuScreen.java
+++ core/src/ru/tds/game/MainMenuScreen.java
@@ -24,46 +24,58 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
  */
 public class MainMenuScreen implements Screen {
 
+    //constants
+    private static final String BACKGROUND = "background2.jpg";
+    private static final String PACK_FILE = "ui/button.pack";
+    private static final String FONT_WHITE = "font/white.fnt";
+    private static final String FONT_BLACK = "font/black.fnt";
+    private static final String BUTTON_UP = "button.up";
+    private static final String BUTTON_DOWN = "button.down";
+    private static final String EXIT = "EXIT";
+    private static final String PLAY = "PLAY";
+    private static final String SETTINGS = "SETTINGS";
+
+    //fields
     private Stage stage;
     private TextureAtlas atlas;
     private Skin skin;
     private Table table;
-    private TextButton buttonPlay, buttonExit;
+    private TextButton buttonPlay, buttonExit, buttonSettings;
     private BitmapFont white, black;
     private Label heading;
     private Texture background;
 
     MainMenuScreen(final SpaceGame spaceGame){
-        background = new Texture("background2.jpg");
+        background = new Texture(BACKGROUND);
         stage = new Stage();
 
         Gdx.input.setInputProcessor(stage);
 
-        atlas = new TextureAtlas("ui/button.pack");
+        atlas = new TextureAtlas(PACK_FILE);
         skin = new Skin(atlas);
 
         table = new Table(skin);
         table.setBounds(0,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
         //creating fonts
-        white = new BitmapFont(Gdx.files.internal("font/white.fnt"), false);
-        black = new BitmapFont(Gdx.files.internal("font/black.fnt"), false);
+        white = new BitmapFont(Gdx.files.internal(FONT_WHITE), false);
+        black = new BitmapFont(Gdx.files.internal(FONT_BLACK), false);
 
         //creating buttons
         TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
-        textButtonStyle.up = skin.getDrawable("button.up");
-        textButtonStyle.down = skin.getDrawable("button.down");
+        textButtonStyle.up = skin.getDrawable(BUTTON_UP);
+        textButtonStyle.down = skin.getDrawable(BUTTON_DOWN);
         textButtonStyle.pressedOffsetX = 1;
         textButtonStyle.pressedOffsetY = -1;
         textButtonStyle.font = black;
 
         //size of button
-        textButtonStyle.up.setMinWidth(300);
-        textButtonStyle.up.setMinHeight(40);
-        textButtonStyle.down.setMinWidth(300);
-        textButtonStyle.down.setMinHeight(40);
+        textButtonStyle.up.setMinWidth(340);
+        textButtonStyle.up.setMinHeight(70);
+        textButtonStyle.down.setMinWidth(340);
+        textButtonStyle.down.setMinHeight(70);
 
-        buttonExit = new TextButton("EXIT", textButtonStyle);
+        buttonExit = new TextButton(EXIT, textButtonStyle);
         buttonExit.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -72,11 +84,20 @@ public class MainMenuScreen implements Screen {
         });
         buttonExit.pad(20);
 
-        buttonPlay = new TextButton("PLAY", textButtonStyle);
+        buttonPlay = new TextButton(PLAY, textButtonStyle);
         buttonPlay.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 ((Game) Gdx.app.getApplicationListener()).setScreen(new GameScreen(spaceGame));
+            }
+        });
+        buttonPlay.pad(20);
+
+        buttonSettings = new TextButton(SETTINGS, textButtonStyle);
+        buttonSettings.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                ((Game) Gdx.app.getApplicationListener()).setScreen(new SettingsScreen(spaceGame));
             }
         });
         buttonPlay.pad(20);
@@ -86,18 +107,17 @@ public class MainMenuScreen implements Screen {
         heading = new Label(SpaceGame.TITLE, headingStyle);
         heading.setFontScale((float) 1.5);
 
+
         //putting stuff together
-        table.add(heading);
-        table.row();
-        table.getCell(heading).spaceBottom(70);
-        table.add(buttonPlay);
-        table.row();
-        table.getCell(buttonPlay).spaceBottom(15);
-        table.add(buttonExit);
+        table.add(heading).spaceBottom(70).row();
+        table.add(buttonPlay).spaceBottom(20).row();
+        table.add(buttonSettings).spaceBottom(20).row();
+        table.add(buttonExit).spaceBottom(20);
         table.setFillParent(true);
         table.background(new TextureRegionDrawable(new TextureRegion(background)));
         stage.addActor(table);
     }
+
 
     @Override
     public void render(float delta) {
@@ -110,11 +130,11 @@ public class MainMenuScreen implements Screen {
 
     }
 
-
-
     @Override
     public void resize(int width, int height) {
-
+        stage.getViewport().update(width, height,true);
+        table.setClip(true);
+        table.setSize(width,height);
     }
 
     @Override
